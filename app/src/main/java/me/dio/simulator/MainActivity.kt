e81293530788341a0e3fbc0a303758bfdc219364
Bruno Carlini <bruno-carlini@hotmail.com>
2022-07-12 22:05:18 -0300

    Update

--- app/src/main/java/me/dio/simulator/MainActivity.kt
+++ app/src/main/java/me/dio/simulator/MainActivity.kt
@@ -4,8 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+
+        //setContentView(R.layout.activity_main)
+        //val tvHello = findViewById<TextView>(R.id.tvHello)
+
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
     }
 }
